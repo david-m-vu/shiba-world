c740d1208eddf05813d96330ba1a046888e137a2
David Vu <david.vu@berkeley.edu>
2026-03-24 16:19:33 -0700

    got rid of body-parser in server, added node engines in package.json

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import { Server } from "socket.io";
 import { registerSocketHandlers } from "./socket/handlers.js";
 import { roomExists } from "./world/rooms.js";
 
+// note render sets an environment variable named PORT for our web service at runtime
 const PORT = Number(process.env.PORT ?? 3001);
 const normalizeOrigin = (value) => {
     try {
