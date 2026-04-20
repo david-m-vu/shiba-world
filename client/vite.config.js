d5a1bdf94872d1f79a81c860bb111f7a5dd3d5bf
David Vu <david.vu@berkeley.edu>
2026-04-19 23:10:45 -0700

    added different message for room invites be setting vercel rewrite to rooms.html on certain links

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import { resolve } from "node:path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        rooms: resolve(__dirname, "rooms.html"),
+      },
+    },
+  },
   plugins: [react(), tailwindcss(), svgr()],
-})
+});
